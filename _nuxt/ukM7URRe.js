d8aaa84f6fe26104a2e208571a864e4dfef8ba8b
mardu <mart.noumees@agileworks.eu>
2024-11-26 21:34:36 +0200

    fourth deploy

---
+++ _nuxt/ukM7URRe.js
@@ -0,0 +1,1 @@
+import{k as a,B as e,u as n,q as p,s as r}from"./Cf6qgKAq.js";const u=a({name:"Paragraph",props:{...e,tag:{type:String,default:"p"}},setup(o,s){const t=n(o);return()=>p(o.tag,r(t),s.slots)}});export{u as _};
